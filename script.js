1fa3ab4ebe02ee58a27ada407095f334d8b36c22
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-08-16 15:12:24 +0300

    add to project

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let weather ={
     apiKey: "b9ee5b0470fbf2a7bdab2e932a32e487",
-    fetchWeather: function (){
+    fetchWeather: function (city){
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q= "
-            + city +
-            "&units=metric&appid="
-             + this.apiKey
+            + city 
+            + "&units=metric&appid="
+            + this.apiKey
         )
         .then ((response) => response.json())
         .then (( data) =>console.log(data));
     },
     displayWeather: function(data){
-        
+
     }
 };
 
