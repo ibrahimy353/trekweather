be9e77ce8dcd21ebe8cc83dd86dd0889ce0baae0
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-08-16 17:12:30 +0300

    add to project

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ let weather ={
         .then ((response) => response.json())
         .then (( data) =>console.log(data));
     },
-    displayWeather: function(data){
-
-    }
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity:" + humidity +"%";
+        document.querySelector(".wind speed").innerText = "Wind speed" + speed + "km/h";
+    
+    },
 };
 
 
